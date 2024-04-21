9f650f7c0e8ccc02d1f7d23f94c69db1995a04ee
shoaky <v_shoaky@outlook.com>
2024-04-21 15:46:25 +0800

    feat: 优化应用重载

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -1,22 +1,49 @@
+<template>
+  <el-menu class="el-menu-demo" mode="horizontal">
+    <el-menu-item index="1">SourceDownloader</el-menu-item>
+    <el-tooltip content="黑夜模式">
+      <el-menu-item h="full" @click="toggleDark()">
+        <button class="border-none w-full bg-transparent cursor-pointer"
+                style="height: var(--ep-menu-item-height)">
+          <i inline-flex i="dark:ep-moon ep-sunny"/>
+        </button>
+      </el-menu-item>
+    </el-tooltip>
+    <el-tooltip content="重载应用">
+      <el-menu-item @click="handleReload()">
+        <el-icon>
+          <refresh/>
+        </el-icon>
+      </el-menu-item>
+    </el-tooltip>
+  </el-menu>
+</template>
+
 <script lang="ts" setup>
 import {toggleDark} from "~/composables";
 import {Refresh} from "@element-plus/icons-vue";
 import {applicationService} from "~/services/data.service";
-</script>
+import {ElLoading, ElMessage} from "element-plus";
 
-<template>
-  <el-menu class="el-menu-demo" mode="horizontal">
-    <el-menu-item index="1">SourceDownloader</el-menu-item>
-    <el-menu-item h="full" @click="toggleDark()">
-      <button class="border-none w-full bg-transparent cursor-pointer"
-              style="height: var(--ep-menu-item-height)">
-        <i inline-flex i="dark:ep-moon ep-sunny"/>
-      </button>
-    </el-menu-item>
-    <el-menu-item @click="applicationService.reload()">
-      <el-icon>
-        <refresh/>
-      </el-icon>
-    </el-menu-item>
-  </el-menu>
-</template>
+const handleReload = async () => {
+  const instance = ElLoading.service({fullscreen: true})
+  await applicationService.reload()
+      .then(_ => {
+        ElMessage({
+          message: '重载完成',
+          type: 'success',
+        })
+      })
+      .catch(err => {
+        console.log(err)
+
+        ElMessage({
+          message: err.response?.data?.detail ?? err.message,
+          type: 'error',
+        })
+      })
+      .finally(() => {
+        instance.close();
+      })
+};
+</script>
