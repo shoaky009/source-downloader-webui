28b8a412f8e2299937e2eb6adff5eec79cda184c
shoaky <v_shoaky@outlook.com>
2024-04-23 02:21:17 +0800

    refactor: 简化组件写法

--- src/views/biz/ShowSourceState.vue
+++ src/views/biz/ShowSourceState.vue
@@ -1,10 +1,9 @@
 <template>
   <el-dialog
-      v-model="showDialog"
+      v-model="isOpen"
       width="500"
-      destroy-on-close
       center
-      @close="handleDialogClose"
+      @open="loadSourceState"
   >
     <json-editor
         :value="data"
@@ -16,33 +15,22 @@
 </template>
 
 <script lang="ts" setup>
+// FIXME 第一次打开会报错
 import {isDark} from "~/composables";
-import {ref, toRefs, watch} from "vue";
+import {ref} from "vue";
 import JsonEditor from 'vue3-ts-jsoneditor';
 import "vue3-json-viewer/dist/index.css";
 import {processorService} from "~/services/data.service";
 
 const props = defineProps<{
   processorName?: string,
-  showState: boolean
 }>()
-
-
-const {processorName, showState} = toRefs(props)
+const isOpen = ref(false)
 const data = ref<any>()
-const showDialog = ref(false)
-watch(showState, async (show) => {
-  if (show && processorName.value) {
-    const response = await processorService.sourceState(processorName.value)
+const loadSourceState = async () => {
+  if (props.processorName) {
+    const response = await processorService.sourceState(props.processorName)
     data.value = response.data.pointer
-    showDialog.value = true
   }
-})
-
-const emit = defineEmits(['dialogClose'])
-const handleDialogClose = () => {
-  showDialog.value = false
-  emit('dialogClose', {showState: false})
 }
-
 </script>
