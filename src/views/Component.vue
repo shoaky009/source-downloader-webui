9022a19ef965286e5b4c0368f23f8b0ba81a46d3
shoaky <v_shoaky@outlook.com>
2024-05-15 01:32:55 +0800

    feat: 展示组件状态

--- src/views/Component.vue
+++ src/views/Component.vue
@@ -1,28 +1,32 @@
 <template>
-  <el-table :data="components" style="width: 100%" v-loading="loading">
-    <el-table-column label="名称" width="180" align="center">
-      <template #default="scope">
-        <span>{{ scope.row.name }}</span>
+  <el-table :data="components"
+            style="width: 100%"
+            v-loading="loading"
+            :row-key="(row) => `${row.type}:${row.typeName}:${row.name}`"
+            :row-class-name="rowClassName"
+            @expand-change="recordExpanded"
+  >
+    <el-table-column type="expand">
+      <template #default="props">
+        <el-row>
+          <el-col :span="24">
+            <div>状态信息:</div>
+            <pre>{{ props.row.stateDetail }}</pre>
+          </el-col>
+        </el-row>
       </template>
     </el-table-column>
-    <el-table-column label="类型" width="180" align="center">
-      <template #default="scope">
-        <span>{{ scope.row.type }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="类型名称" width="180" align="center">
-      <template #default="scope">
-        <span>{{ scope.row.typeName }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="引用" label="Refs" align="center">
+    <el-table-column label="类型" width="180" align="center" prop="type"/>
+    <el-table-column label="类型名称" width="180" align="center" prop="typeName"/>
+    <el-table-column label="名称" width="180" align="center" prop="name"/>
+    <el-table-column label="引用" align="center">
       <template #default="scope">
         <el-tag v-for="item in scope.row.refs">
           {{ item }}
         </el-tag>
       </template>
     </el-table-column>
-    <el-table-column label="操作">
+    <el-table-column label="操作" align="center">
       <template #default="scope">
         <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
           WIP编辑
@@ -43,12 +47,17 @@
 </template>
 
 <script setup lang="ts">
-import {ElButton, ElTag,} from "element-plus";
-import {ref, onMounted} from "vue";
+import {ElButton, ElTag} from "element-plus";
+import {onMounted, ref} from "vue";
 import {Component, componentService} from "~/services/data.service";
 
 const loading = ref(false)
 const components = ref<Component[]>([])
+const rowClassName = ({row}: { row: Component }) => {
+  if (row.stateDetail == null) {
+    return 'row-expand-cover'
+  }
+}
 
 const loadMore = async () => {
   loading.value = true
@@ -72,4 +81,39 @@ const handleDelete = (index: number, component: Component) => {
 const handleReload = async (component: Component) => {
   await componentService.reload(component)
 }
+
+// state stream
+const handleStateUpdate = (event: MessageEvent) => {
+  const [type, typeName, name] = event.lastEventId.split(":")
+  components.value.filter((component) => {
+    return component.type === type && component.typeName === typeName && component.name === name
+  }).forEach((component) => {
+    component.stateDetail = JSON.parse(event.data)
+  })
+}
+
+let eventSource: EventSource | null = null
+const recordExpanded = (_: Component, expandedRows: Component[]) => {
+  if (eventSource) {
+    eventSource.close()
+  }
+
+  const id = expandedRows.map(row => {
+    return `${row.type}:${row.typeName}:${row.name}`
+  })
+  if (id.length == 0) {
+    return
+  }
+  eventSource = componentService.stateStream(id)
+  if (eventSource) {
+    eventSource.addEventListener('component-state', handleStateUpdate);
+  }
+}
+
 </script>
+
+<style>
+.row-expand-cover .ep-table__expand-icon {
+  visibility: hidden;
+}
+</style>
